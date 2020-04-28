0607c8668de81b3535cfe3cda1b5100c09023354
sumit pandey <pandeysumit22121997@gmail.com>
2020-04-28 14:42:58 +0530

    Updates

---
+++ precache-manifest.8c52d767b228e804df6e07043e15bb4f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e83594f7365b8bf7ac870bec6dcb272",
+    "url": "/deploy_react/index.html"
+  },
+  {
+    "revision": "9716f671e13c12f44736",
+    "url": "/deploy_react/static/css/2.32e6b33e.chunk.css"
+  },
+  {
+    "revision": "59922efef60ba27c82ed",
+    "url": "/deploy_react/static/css/main.7d967c12.chunk.css"
+  },
+  {
+    "revision": "9716f671e13c12f44736",
+    "url": "/deploy_react/static/js/2.75c2b094.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/deploy_react/static/js/2.75c2b094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59922efef60ba27c82ed",
+    "url": "/deploy_react/static/js/main.78b3c7c4.chunk.js"
+  },
+  {
+    "revision": "a773a2debf799c82e6f6",
+    "url": "/deploy_react/static/js/runtime-main.2c80c50e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/deploy_react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/deploy_react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/deploy_react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deploy_react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/deploy_react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/deploy_react/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/deploy_react/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/deploy_react/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/deploy_react/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/deploy_react/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
